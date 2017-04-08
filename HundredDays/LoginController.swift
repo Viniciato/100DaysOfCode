a9a0a982d561680fa23a41601530984b40ce5d3f
Vinicius Nadin <vininadin@gmail.com>
2017-04-08 15:38:37 -0300

    Added login Functionality

--- HundredDays/LoginController.swift
+++ HundredDays/LoginController.swift
@@ -9,11 +9,23 @@
 import UIKit
 import Firebase
 
-class LoginController: UIViewController {
-
+class LoginController: UIViewController, UITextFieldDelegate {
+    
+    // MARK : - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    // MARK : - Properties
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    // MARK : - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setupTextFields()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,11 +33,95 @@ class LoginController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK : - View Methods
+    func setupTextFields() {
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+        self.emailTextField.autocorrectionType = .no
+        self.emailTextField.keyboardType = .emailAddress
+        self.passwordTextField.isSecureTextEntry = true
+        self.passwordTextField.autocorrectionType = .no
+    }
+    
+    func verifyField(textField : UITextField) -> Bool{
+        var fieldValue = textField.text
+        if !(fieldValue?.isEmpty)! {
+            if textField == self.emailTextField {
+                fieldValue = fieldValue?.replacingOccurrences(of: " ", with: "")
+            }
+            return true
+        }
+        MissingFieldAlert.showAlert(vc: self, title: "Erro", message: "Por favor preencha os campos vazios")
+        return false
+    }
+    
+    // MARK : - View Actions
+    
     @IBAction func goTwo(_ sender: UIButton) {
                 UIApplication.shared.keyWindow?.rootViewController = storyboard!.instantiateViewController(withIdentifier: "HomeViewController")
     }
     
+    @IBAction func tryToLogin(_ sender: UIButton) {
+        if self.verifyField(textField: self.emailTextField) && self.verifyField(textField: self.passwordTextField){
+            let email = self.emailTextField.text
+            let password = self.passwordTextField.text
+            FIRAuth.auth()?.signIn(withEmail: email!, password: password!, completion: { (user, error) in
+                if error != nil {
+                    if let errCode = FIRAuthErrorCode(rawValue: error!._code){
+                        var message = ""
+                        switch errCode{
+                        case .errorCodeInvalidEmail:
+                            message = "Por favor inserir um endereco de email valido!"
+                        case .errorCodeUserNotFound:
+                            message = "Usuario nao encontrado!!"
+                        case .errorCodeWrongPassword:
+                            message = "Senha incorreta!!"
+                        default:
+                            message = "\(error!)"
+                        }
+                        MissingFieldAlert.showAlert(vc: self, title: "Erro!", message: message)
+                    }
+                    return
+                }
+                self.dismiss(animated: true, completion: nil)
+            })
+        }
+    }
 
+    // MARK : - Overrided Methods
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.becomeFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.emailTextField {
+            self.passwordTextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
